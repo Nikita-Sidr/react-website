3d78ddeb26ab51ccfcda648a23b6787aa4c2ae2d
Nikita Sydorenko <sandorik2001@gmail.com>
2023-05-18 09:21:52 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+          <img src='https://cdn.wallpapersafari.com/44/54/F4oMUk.jpg' alt=''/>
+          <div>     
+            ava + description     
+          </div>
+          <div>
+            My posts
+            <div>
+              new post
+            </div>
+            <div>
+              <div>
+                post 1
+              </div>
+              <div>
+                post 2
+              </div>
+            </div>
+          </div>
+        </div>
+    )
+}
+export default Profile
