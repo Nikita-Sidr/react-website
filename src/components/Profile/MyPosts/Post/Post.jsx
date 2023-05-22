856f3571106ee45ddd665bb9b3a8da3d009ca7ee
Nikita Sydorenko <sandorik2001@gmail.com>
2023-05-22 11:49:44 +0300

    modernised post

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import s from './Post.module.css'
 const Post = () => {
   return (
-        <div className={s.item}>
-          <img src="https://amiel.club/uploads/posts/2022-03/1647747400_3-amiel-club-p-kartinki-tigra-na-avu-3.jpg" alt=""/>
-          post 1
+    <div className={s.item}>
+      <img src="https://amiel.club/uploads/posts/2022-03/1647747400_3-amiel-club-p-kartinki-tigra-na-avu-3.jpg" alt="" />
+      post 1
 
-          <div>
-          <span>like</span>
-          </div>
-          
-         </div>
+      <div>
+        <span>like</span>
+      </div>
+
+    </div>
   )
 }
 export default Post
