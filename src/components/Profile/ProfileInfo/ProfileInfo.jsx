31be1bb5ddda3f8754caccf16253f527415b9b5f
NIKITA SYDORENKO <sandorik2001@gmail.com>
2023-08-28 15:25:43 +0300

    updated with hooks useState useEffect

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import s from './ProfileInfo.module.css'
 import Preloader from "../../Common/Preloader/Preloader";
 import ProfileStatus from './ProfileStatus'
+import ProfileStatusWithHooks from './ProfileStatusWithHooks'
 
 const ProfileInfo = (props) => {
 
@@ -19,7 +20,7 @@ const ProfileInfo = (props) => {
         <div>name : {props.profile.fullName}</div>
         <div>About Me : {props.profile.aboutMe}</div>
         <div>Profile Status 
-        <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
         </div>
       </div>
     </div>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -78,6 +78,7 @@ export const toggleFollowingProgress = (isFetching, userId) => ({ type: TOGGLE_I
 export const requestUsers =  (currentPage,pageSize) => {
     return (dispatch) => {
         dispatch(toggleIsFetching(true))
+        dispatch(setCurrentPage(currentPage))
     
         usersAPI.getUsers(currentPage, pageSize)
             .then(data => {
